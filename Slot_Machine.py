0e18b5df8a7e3cceebb5083ec754091d90dc8719
adihora7 <aadityadihora7@gmail.com>
2026-02-06 14:09:01 -0500

    implemented the machine

--- Slot_Machine.py
+++ Slot_Machine.py
@@ -1,6 +1,61 @@
+import random
+
 MAX_LINES = 3
 MAX_BET = 100
 MIN_BET = 1
+
+ROWS = 3
+COLS = 3
+
+symbol_count = {
+    "A": 2,
+    "B": 4,
+    "C": 6,
+    "D": 8
+}
+
+symbol_count = {
+    "A": 5,
+    "B": 4,
+    "C": 3,
+    "D": 2
+}
+
+
+def check_winnings(columns, lines, bet, values):
+    for line in range(lines):
+        symbol = columns[0][line]
+
+
+def get_slot_machine_spin(rows, cols, symbols):
+    all_symbols = []
+    for symbol, symbol_count in symbols.items():
+        for _ in range(symbol_count):
+            all_symbols.append(symbol)
+
+    columns = []
+    for _ in range(cols):
+        column = []
+        current_symbols = all_symbols[:]
+        for _ in range(rows):
+            value = random.choice(current_symbols)
+            current_symbols.remove(value)
+            column.append(value)
+
+        columns.append(column)
+
+    return columns
+
+
+def print_slot_machine(columns):
+    for row in range(len(columns[0])):
+        for i, column in enumerate(columns):
+            if i != len(columns) - 1:
+                print(column[row], end=' | ')
+            else:
+                print(column[row], end='')
+
+        print()
 
 
 def deposit():
@@ -21,8 +76,8 @@ def deposit():
 def get_number_of_lines():
     while True:
         try:
-            lines = float(
-                input('Enter the number of lines to bet on (1- ' + str(MAX_LINES) + ')? '))
+            lines = int(
+                input('Enter the number of lines to bet on (1-' + str(MAX_LINES) + ')? '))
             if 1 <= lines <= MAX_LINES:
                 break
             else:
@@ -51,9 +106,21 @@ def get_bet():
 def main():
     balance = deposit()
     lines = get_number_of_lines()
-    bet = get_bet()
+    while True:
+        bet = get_bet()
+        total_bet = round(bet*lines, 2)
 
-    print(balance, lines)
+        if total_bet > balance:
+            print(
+                f'You do not have enough to bet that amount, your current balance is ${balance}')
+        else:
+            break
+
+    print(
+        f'You are betting ${bet} on {lines} lines. Total bet is equal to ${total_bet}')
+
+    slots = get_slot_machine_spin(ROWS, COLS, symbol_count)
+    print_slot_machine(slots)
 
 
 main()
